d9b2a44d5e98176598380089e8a14df2b77e936e
房召谋 <1079336302@qq.com>
2017-12-28 15:41:17 +0800

    init

---
+++ src/page/Home/Home.vue
@@ -0,0 +1,21 @@
+<style scoped>
+  .Home {
+
+  }
+</style>
+
+<template>
+  <div class="Home">
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
